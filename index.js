8e6b1bd9266e5ce9ef68cda3dd13efce4fdd4682
Patrick Lebsack <p_lebbysack@hotmail.com>
2019-02-09 02:13:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string)
   return "I can't hear you!";
 }
-function sayHiToGrandma(string) {
   if (string.toUpperCase() === string)
   return "YES INDEED!";
 }
